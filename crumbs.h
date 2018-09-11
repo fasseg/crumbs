fb6b61a57f5b4df1003b04d629eb87a4b62fe275
Frank Asseg <frank.asseg@dm.de>
2018-09-11 07:57:52 +0200

    Revert "Uses sysconfdir for config file location."
    This reverts commit 1861dc06907f32ca7456515c40ed613ed974cfb1.

--- crumbs.h
+++ crumbs.h
@@ -21,7 +21,7 @@
 #define CRUMBEXISTS -1
 #define EDIREXISTS -2
 
-#define DEFAULT_CONFIG SYSCONFDIR "/crumbs.conf"
+#define DEFAULT_CONFIG "/etc/crumbs.conf"
 
 struct crumb_config {
     char *path;
